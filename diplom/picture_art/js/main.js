371bb21f94536d63dcee073e37aad0c61e52a882
Kriss <kiril_winner@mail.ru>
2018-09-11 13:35:13 +0300

    -calc+gambg

--- diplom/picture_art/js/main.js
+++ diplom/picture_art/js/main.js
@@ -284,7 +284,96 @@ portfolioMenu.addEventListener('click', function(e) {
 		styleBlockButton.style.display = 'none';
 	});
 
+		// гамбургер
 
+	let mobileBtn = document.getElementById('burger'),
+			mobileMenu = document.getElementById('burger-menu'),
+			headerMenu = document.getElementById('header-menu');
+
+	function mobileBtnCheck() {
+		if (screen.width > 991) {
+			mobileBtn.style.display = 'none';
+			mobileMenu.style.display = 'none';
+			headerMenu.style.display = 'block';
+		} else{
+			mobileBtn.style.display = 'block';
+			headerMenu.style.display = 'none';
+			mobileMenu.style.display = 'none';				
+		}
+	}
+
+	mobileBtnCheck();
+
+	window.addEventListener('resize', mobileBtnCheck);
+
+	mobileBtn.addEventListener('click', function(){
+		if (mobileMenu.style.display == 'block') {
+			mobileMenu.classList.add('hinge');
+			setTimeout(function(){
+				mobileMenu.style.display = 'none';
+			}, 90);
+		} else {
+			mobileMenu.style.display = 'block';
+			mobileMenu.classList.remove('hinge');
+		}		
+	});
+
+	// калькулятор
+
+	let size = document.getElementById('size'),
+			material = document.getElementById('material'),
+			options = document.getElementById('options'),
+			promocode = document.getElementById('promocode'),
+			calcPriceValue = document.getElementById('calc-price-value'),
+			calcPriceText = document.getElementById('calc-price-none'),
+			buttonOrderCalc = document.getElementById('button-order-calc'),
+			price = 0,
+			work = 5000;
+
+
+	function handleChange () {
+		if (size.selectedIndex === '' || material.selectedIndex === '') {
+			calcPriceText.style.display = 'block';
+			calcPriceValue.style.display = 'none';
+		} else if (options.selectedIndex === '' && promocode.value === ''){
+			price = parseInt(size.value) + parseInt(material.value) + work;
+			calcPriceText.style.display = 'none';
+			calcPriceValue.style.display = 'block';
+			calcPriceValue.innerHTML = price;
+		} else if (options.selectedIndex !== '' && promocode.value === ''){
+			price = parseInt(size.value) + parseInt(material.value) + parseInt(options.value) + work;
+			calcPriceText.style.display = 'none';
+			calcPriceValue.style.display = 'block';
+			calcPriceValue.innerHTML = price;
+		} else if (options.selectedIndex === '' && promocode.value === 'IWANTPOPART'){
+			let discount = Math.round(((parseInt(size.value) + parseInt(material.value) + work)*30)/100);
+			price = (parseInt(size.value) + parseInt(material.value) + work);
+			calcPriceText.style.display = 'none';
+			calcPriceValue.innerHTML = price - discount;
+		} else {
+			let discount = Math.round(((parseInt(size.value) + parseInt(material.value) + parseInt(options.value) + work)*30)/100);
+			price = parseInt(size.value) + parseInt(material.value) + parseInt(options.value) + work;
+			calcPriceText.style.display = 'none';
+			calcPriceValue.style.display = 'block';
+			calcPriceValue.innerHTML = price - discount;
+		}
+	}
+
+	size.addEventListener('change', function(){
+		handleChange ();
+	});
+
+	material.addEventListener('change', function(){
+		handleChange ();
+	});
+
+	options.addEventListener('change', function(){
+		handleChange ();
+	});
+
+	promocode.addEventListener('change', function(){
+		handleChange ();
+	});
 	
 
 
